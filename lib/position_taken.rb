5dd3fd2870fa9c7024eece2abee9175874087d75
Aude Faucheux <aude53@gmail.com>
2019-07-14 08:54:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] != " "
-    true
-  elsif board[index] != ""
+  if board[index] != " " || ""
     true
   else false
   end
